f7d04c239b67933ea57586508fc80fb1fbe0e3d0
Niggel A <56854482+El-NyaX3M@users.noreply.github.com>
2022-06-06 22:17:36 -0600

    Add files via upload

---
+++ editarPerfil.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/perfil.css">
+    <title>Krachtig Gym - Editar Perfil</title>
+</head>
+<body style="display: grid;">
+    <?php include("templates/tmplt_header.php");?>
+    <section>
+        <div class="div-name">
+            <label class="name" id="idNombre">Nombre</label>
+        </div>
+        <div class="div-info">
+            <div class="info">
+                <ul>
+                    <li><label class="info-text">Membresía: </label><label class="info-text bold">#membresia</label></li>
+                    <li><label class="info-text">Miembro desde: </label><label class="info-text bold">#fechamiembro</label></li>
+                    <br>
+                    <li><label class="info-text">Vigencia: </label><label class="info-text bold">#vigencia</label></li>
+                    <br>
+                    <li><label class="info-text bold">Sexo: </label><select class="info-text">
+                        <option value="Masculino">Masculino</option>
+                        <option value="Femenino">Femenino</option>
+                        <option value="Otro">Otro</option>
+                    </select></li>
+                    <li><label class="info-text bold">Fecha de nacimiento: </label><input type="date" class="info-text"/></li>
+                    <li><label class="info-text bold">Tipo de sangre: </label><select class="info-text">
+                        <option value="A+">A+</option>
+                        <option value="A-">A+</option>
+                        <option value="B+">A+</option>
+                        <option value="B-">A+</option>
+                        <option value="AB+">A+</option>
+                        <option value="AB-">A+</option>
+                        <option value="O+">A+</option>
+                        <option value="O-">A+</option>
+                    </select></li>
+                    <br>
+                    <li><label class="info-text bold">Teléfono: </label><input type="tel" class="info-text"/></li>
+                    <li><label class="info-text bold">Correo: </label><input type="mail" class="info-text"/></li>
+                    <br>
+                    <li><label class="info-text bold">Dirección </label></li>
+                    <li><label class="info-text bold">Calle: </label><input type="text" class="info-text"/></li>
+                    <li><label class="info-text bold">Colonia: </label><input type="text" class="info-text"/></li>
+                    <li><label class="info-text bold">Código postal: </label><input type="number" class="info-text"/></li>
+                </ul>
+            </div>
+            <div class="visits">
+                <a href="perfil.php"><button>Terminar</button></a>
+            </div>
+        </div>
+    </section>
+    <?php include("templates/tmplt_footer.php");?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Krachtig Gym - Ingresar</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body style="background-color: rgba(6, 17, 60, 1);">
+	<section class="contenedor">
+		<div>
+			<form>
+				<div class="formulario">
+					<h3 style="color: rgb(255, 139, 44);">Iniciar sesión</h3>
+					<input type="text" id="inpCorreo" class="inputLogin" placeholder="Correo electrónico">
+					<br>
+					<input type="password" id="inpContras" class="inputLogin" placeholder="Contraseña">
+					<br>
+					<button id="btnIngresar">
+						<h3 style="color: white;">Ingresar</h3>
+					</button>
+				</div>
+			</form>
+		</div>
+	</section>
+</body>
+</html>
