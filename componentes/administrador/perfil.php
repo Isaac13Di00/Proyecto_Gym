ad87728371bb5485ad4eaffe0fd4e7929c589aa8
Isaac Pérez <56978632+Isaac13Di00@users.noreply.github.com>
2022-06-08 14:09:23 -0600

    Actualizacion carpetas

---
+++ componentes/administrador/perfil.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+    error_reporting(0); 
+    if (!isset($_SESSION['nombre'])) {
+        header("Location: /Proyecto_Gym/");
+    }    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Krachtig Gym - Perfil</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="../../css/perfil.css">
+    <title>Document</title>
+</head>
+<body style="display: grid;">
+    <?php include("./templates/tmplt_header.php");?>
+    <section>
+        <div class="div-name">
+            <label class="name" id="idNombre"><?php echo $_SESSION['nombre'];?></label>
+        </div>
+        <div class="div-info">
+            <div class="info">
+                <ul>
+                    <li><label class="info-text">Membresía: </label><label class="info-text bold"><?php echo $_SESSION['membresia'];?></label></li>
+                    <li><label class="info-text">Miembro desde: </label><label class="info-text bold"><?php echo $_SESSION['miembro_desde'];?></label></li>
+                    <br>
+                    <li><label class="info-text">Vigencia: </label><label class="info-text bold"><?php echo $_SESSION['vigencia'];?></label></li>
+                    <br>
+                    <li><label class="info-text bold">Sexo: </label><label class="info-text"><?php echo $_SESSION['sexo'];?></label></li>
+                    <li><label class="info-text bold">Fecha de nacimiento: </label><label class="info-text"><?php echo $_SESSION['fecha_nacimiento'];?></label></li>
+                    <li><label class="info-text bold">Tipo de sangre: </label><label class="info-text"><?php echo $_SESSION['tipo_sangre'];?></label></li>
+                    <br>
+                    <li><label class="info-text bold">Teléfono: </label><label class="info-text"><?php echo $_SESSION['telefono'];?></label></li>
+                    <li><label class="info-text bold">Correo: </label><label class="info-text"><?php echo $_SESSION['correo'];?></label></li>
+                    <br>
+                    <li><label class="info-text bold">Dirección </label></li>
+                    <li><label class="info-text bold">Calle: </label><label class="info-text"><?php echo $_SESSION['calle'];?></label></li>
+                    <li><label class="info-text bold">Colonia: </label><label class="info-text"><?php echo $_SESSION['colonia'];?></label></li>
+                    <li><label class="info-text bold">Código postal: </label><label class="info-text"><?php echo $_SESSION['cp'];?></label></li>
+                </ul>
+            </div>
+            <div class="visits">
+                <a href="./editarPerfil.php"><button>Editar perfil</button></a>
+            </div>
+        </div>
+    </section>
+    <?php include("./templates/tmplt_footer.php");?>
+</body>
+</html>
---
+++ componentes/administrador/templates/tmplt_header.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="/Proyecto_Gym/css/headerStyle.css">
+<header>
+        <div class="header-pageName">
+            <h1>KRACHTIG GYM</h1>
+        </div>
+        <div class="more">
+                <button class="mas">Mas</button>
+            </div>
+</header>
+<div class="header-more">
+            <div class="tabs">
+                <ul>
+                    <a href="../usuario/perfil.php"><li>Perfil</li></a>
+                    <a href="../usuario/agendar.php"><li>Agendar</li></a>
+                    <a><li>Historial</li></a>
+                </ul>
+            </div>
+        </div>
